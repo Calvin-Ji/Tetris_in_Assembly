5bb927eb39276d26c49c34bb879b028f2817d879
Calvin Ji <81500827+Calvin-Ji@users.noreply.github.com>
2024-03-22 18:49:31 -0400

    Add files via upload

---
+++ Assembly_Tetris/tetris.asm
@@ -0,0 +1,51 @@
+################ CSC258H1F Winter 2024 Assembly Final Project ##################
+# This file contains our implementation of Tetris.
+#
+# Student 1: Name, Student Number
+# Student 2: Name, Student Number (if applicable)
+######################## Bitmap Display Configuration ########################
+# - Unit width in pixels:       2
+# - Unit height in pixels:      2
+# - Display width in pixels:    64
+# - Display height in pixels:   64
+# - Base Address for Display:   0x10008000 ($gp)
+##############################################################################
+
+    .data
+##############################################################################
+# Immutable Data
+##############################################################################
+# The address of the bitmap display. Don't forget to connect it!
+ADDR_DSPL:
+    .word 0x10008000
+# The address of the keyboard. Don't forget to connect it!
+ADDR_KBRD:
+    .word 0xffff0000
+
+##############################################################################
+# Mutable Data
+##############################################################################
+red: 0x00ff0000
+green: 0x0000ff00
+blue: 0x0000000ff
+##############################################################################
+# Code
+##############################################################################
+	.text
+	.globl main
+
+	# Run the Tetris game.
+main:
+    
+    # Initialize the game
+
+game_loop:
+	# 1a. Check if key has been pressed
+    # 1b. Check which key has been pressed
+    # 2a. Check for collisions
+	# 2b. Update locations (paddle, ball)
+	# 3. Draw the screen
+	# 4. Sleep
+
+    #5. Go back to 1
+    b game_loop
